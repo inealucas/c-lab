0d992a789838a4487ff62ba77307343aa8a84935
Lucas Sousa <lucas537023@gmail.com>
2026-06-12 02:20:25 -0300

    add hour-based mood checker

---
+++ c/clock_mod.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int hour = 14;
+
+    if (hour < 6) {
+        printf("You should be asleep.\n");
+    } else if (hour < 12) {
+        printf("Morning. Coffee first.\n");
+    } else if (hour < 18) {
+        printf("Afternoon. Probably procrastinating.\n");
+    } else if (hour < 22) {
+        printf("Evening. Almost done.\n");
+    } else {
+        printf("Go to bed.\n");
+    }
+
+    return 0;
+}
