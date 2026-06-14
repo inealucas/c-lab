ad5d95d5155070c02a56b666ce9eb0dc336ceb0a
Lucas Sousa <lucas537023@gmail.com>
2026-06-14 00:43:01 -0300

    feat: add age validation for signup

---
+++ c/age_check.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+
+    
+    int age;
+
+   printf("Enter your age: ");
+   scanf("%d", &age);
+
+  if (age >= 18) {
+   printf("You are now singed up!");
+  } 
+  else if (age == 0) {
+    printf("You cant singed up! You were just born");
+    
+  }
+  else if (age < 0){
+   printf("You haven't been born yet!");
+  }
+  else {
+    printf("You are too young to singed up!");
+  }
+
+    return 0;
+}
